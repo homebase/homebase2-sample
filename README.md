a17080da1f8379f35a2ac50ea73cf701c0ed5abf
Sergey Porfiriev <parf@comfi.com>
2018-03-22 15:49:38 -0400

    x

---
+++ README.md
@@ -0,0 +1,3 @@
+Sample App for Homebase 2 php 7.2 framework
+
+@see https://github.com/homebase/homebase2/wiki/DirectoryStructure
--- init.php
+++ init.php
@@ -1,6 +1,5 @@
 <?php
 
 define("APP_DIR", __DIR__);
-include __DIR__."/vendor/homebase2/src/homebase-bundle.inc.php";  // homebase project init
+include __DIR__."/vendor/hb2/src/homebase-bundle.inc.php";  // homebase project init
 \hb\HB::dispatch();
-
