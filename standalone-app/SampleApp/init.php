a604ccf08bc8a6a458f6d263a1de3c7c070e7258
Serg Parf <sergey.porfiriev@gmail.com>
2018-04-02 22:32:12 -0400

    config-minor

--- standalone-app/SampleApp/init.php
+++ standalone-app/SampleApp/init.php
@@ -1,5 +1,5 @@
 <?php
 
 define("APP_DIR", __DIR__);
-include __DIR__."/vendor/hb2/src/homebase-bundle.inc.php";  // homebase project init
+include "/proj/vendor/homebase2/src/homebase-bundle.inc.php";  // homebase project init
 \hb\HB::dispatch();
